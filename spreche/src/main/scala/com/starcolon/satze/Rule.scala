160a7ee1dc6321b2927875a832d3dcd99d40b746
starcolon <tao@starcolon.com>
2019-03-17 19:57:58 +0100

    add initial components

---
+++ spreche/src/main/scala/com/starcolon/satze/Rule.scala
@@ -0,0 +1,23 @@
+package com.starcolon.satze
+
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+
+class Conjugation(
+  ich: String,
+  du: String,
+  er: String,
+  sie: String,
+  es: String,
+  wir: String,
+  ihr: String,
+  Sie: String
+)
+
+class Ort
+
+class Rule 
+
+object Rule {
+  def loadContext: Rule = ???
+}
