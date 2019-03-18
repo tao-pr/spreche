e2914305471817682d5cb97f60300449751ad513
starcolon <tao@starcolon.com>
2019-03-18 22:54:24 +0100

    parsing conjugation rule

--- spreche/src/main/scala/com/starcolon/satze/Satze.scala
+++ spreche/src/main/scala/com/starcolon/satze/Satze.scala
@@ -1,13 +1,14 @@
 package com.starcolon.satze
 
-import com.starcolon.satze.Rule
+import com.starcolon.satze.{Rule, FrageWort}
+import com.starcolon.satze.{Wie, Wo, Warum, Wann, Wer}
 
 class Satze(val tokens: Seq[Token]) {
   override def toString: String = ???
-  def toFrage(frageWort: String): Frage = ???
+  def toFrage(frageWort: FrageWort): Frage = ???
 }
 
-class Frage(override val tokens: Seq[Token], val frageWort: String) extends Satze(tokens) {
+class Frage(override val tokens: Seq[Token], val frageWort: FrageWort) extends Satze(tokens) {
   override def toString = ???
   def toSatze: Satze = ???
 }
