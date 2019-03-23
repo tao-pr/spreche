cd06b2ff752049d47565503decf2ef77049fb406
Tao PR <pataoengineer@gmail.com>
2019-03-23 21:48:41 +0100

    complete missing implementations

--- spreche/src/main/scala/com/starcolon/satze/Sprechen.scala
+++ spreche/src/main/scala/com/starcolon/satze/Sprechen.scala
@@ -1,11 +1,12 @@
 package com.starcolon.satze
 
+import java.io._
 import com.starcolon.satze._
 
 object Sprechen {
   def conversation(implicit rule: Rule): Unit = {
     // Generate a frage
-    val a = readInput("Sagen Sie > ")
+    val a = readLine("Sagen Sie > ")
 
 
     conversation
