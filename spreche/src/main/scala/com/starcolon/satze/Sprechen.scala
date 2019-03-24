4096e3c94684458bfeeaf73ab37c254de3e73be9
Tao PR <pataoengineer@gmail.com>
2019-03-24 09:31:15 +0100

    wip: prototype of tokens parser

--- spreche/src/main/scala/com/starcolon/satze/Sprechen.scala
+++ spreche/src/main/scala/com/starcolon/satze/Sprechen.scala
@@ -4,9 +4,9 @@ import java.io._
 import com.starcolon.satze._
 
 object Sprechen {
-  def conversation(implicit rule: Rule): Unit = {
-    val ein = readLine("Sagen Sie > ").split(" ").filter(_.trim.size > 0)
-
+  def conversation(implicit rule: MasterRule): Unit = {
+    val inputTokens = readLine("Sagen Sie > ").split(" ").filter(_.trim.size > 0)
+    val tokens = Satze.parse(inputTokens)
 
     conversation
   }
