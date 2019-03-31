789b0b722d31c86ff8a9720c3e4cbfde8f91f4ae
starcolon <tao@starcolon.com>
2019-03-31 23:54:55 +0200

    speak

--- spreche/src/main/scala/com/starcolon/satze/Implicits.scala
+++ spreche/src/main/scala/com/starcolon/satze/Implicits.scala
@@ -1,8 +1,13 @@
 package com.starcolon.satze
 
+import sys.process._
+
+
 object Implicits {
   implicit class StrOps(val s: String) extends AnyVal {
     def capInitial = if (s.trim.isEmpty) s.trim else s.take(1).toUpperCase + s.tail.toLowerCase
+
+    def speak = ("say -v Anna " + s) !
   }
 }
   
--- spreche/src/main/scala/com/starcolon/satze/Sprechen.scala
+++ spreche/src/main/scala/com/starcolon/satze/Sprechen.scala
@@ -2,13 +2,16 @@ package com.starcolon.satze
 
 import com.starcolon.satze._
 import Console._
+import com.starcolon.satze.Implicits._
 
 object Sprechen {
   def conversation(implicit rule: MasterRule): Unit = {
     val inputTokens = readLine("Sagen Sie > ").split(" ").filter(_.trim.size > 0).toList
     val inputSatze = Satze.parse(inputTokens)
     println(inputSatze)
-    println(s"${MAGENTA}${inputSatze.render()}${RESET}")
+    val out = inputSatze.render()
+    println(s"${MAGENTA}${out}${RESET}")
+    out.speak
     conversation
   }
 }
